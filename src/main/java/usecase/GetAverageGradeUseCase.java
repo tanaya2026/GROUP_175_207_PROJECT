5fd87941b0022b515e128b8ee61b1167f103347d
cbolinius <cooperbolinius@gmail.com>
2024-11-07 16:23:11 -0500

    Started implementation of SlotifyDataBase Implemented CreateResourceUseCase and test Commented out all incomplete/problamatic code to make program buildable Resource creation on Slotify via API in IntelliJ successful

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -1,41 +1,41 @@
-package usecase;
-
-import api.GradeDataBase;
-import entity.Grade;
-import entity.Availability;
-
-/**
- * GetAverageGradeUseCase class.
- */
-public final class GetAverageGradeUseCase {
-    private final GradeDataBase gradeDataBase;
-
-    public GetAverageGradeUseCase(GradeDataBase gradeDataBase) {
-        this.gradeDataBase = gradeDataBase;
-    }
-
-    /**
-     * Get the average grade for a course across your team.
-     * @param course The course.
-     * @return The average grade.
-     */
-    public float getAverageGrade(String course) {
-        // Call the API to get usernames of all your team members
-        float sum = 0;
-        int count = 0;
-        final Availability team = gradeDataBase.getMyTeam();
-        // Call the API to get all the grades for the course for all your team members
-
-        for (String user : team.getMembers()) {
-            Grade userGrade = gradeDataBase.getGrade(user, course);
-            sum += userGrade.getGrade();
-        }
-
-        count = team.getMembers().length;
-
-        if (count == 0) {
-            return 0;
-        }
-        return sum / count;
-    }
-}
+//package usecase;
+//
+//import api.GradeDataBase;
+//import entity.Grade;
+//import entity.Availability;
+//
+///**
+// * GetAverageGradeUseCase class.
+// */
+//public final class GetAverageGradeUseCase {
+//    private final GradeDataBase gradeDataBase;
+//
+//    public GetAverageGradeUseCase(GradeDataBase gradeDataBase) {
+//        this.gradeDataBase = gradeDataBase;
+//    }
+//
+//    /**
+//     * Get the average grade for a course across your team.
+//     * @param course The course.
+//     * @return The average grade.
+//     */
+//    public float getAverageGrade(String course) {
+//        // Call the API to get usernames of all your team members
+//        float sum = 0;
+//        int count = 0;
+//        final Availability team = gradeDataBase.getMyTeam();
+//        // Call the API to get all the grades for the course for all your team members
+//
+//        for (String user : team.getMembers()) {
+//            Grade userGrade = gradeDataBase.getGrade(user, course);
+//            sum += userGrade.getGrade();
+//        }
+//
+//        count = team.getMembers().length;
+//
+//        if (count == 0) {
+//            return 0;
+//        }
+//        return sum / count;
+//    }
+//}
