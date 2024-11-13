8c5b8605fdd0681a0e79e02a12b4c0de419cb922
cbolinius <cooperbolinius@gmail.com>
2024-11-12 20:00:47 -0500

    Changed User constructor Started working on creating a new scheduler with the API Added a use case for creating a new scheduler

--- src/test/java/CreateResourceUseCaseMockitoTest.java
+++ src/test/java/CreateResourceUseCaseMockitoTest.java
@@ -43,59 +43,7 @@ public class CreateResourceUseCaseMockitoTest {
         User testUser = new User("abc", "abc@gmail.com", "123"
         , "Jamie Jolly", testCourses, "Biochem", "hello!");
 
-        // createResourceUseCase.createSlotifyResource(testUser);
-
         assertDoesNotThrow(() -> createResourceUseCase.createSlotifyResource(testUser));
 
-
-//        Grade[] expectedTeammember1Grades = new Grade[3];
-//        expectedTeammember1Grades[0] = Grade.builder()
-//                .username("t1chenpa")
-//                .course("CSC207")
-//                .grade(85)
-//                .build();
-//        expectedTeammember1Grades[1] = Grade.builder()
-//                .username("t1chenpa")
-//                .course("CSC148")
-//                .grade(86)
-//                .build();
-//        expectedTeammember1Grades[2] = Grade.builder()
-//                .username("t1chenpa")
-//                .course("CSC165")
-//                .grade(91)
-//                .build();
-//
-//        Grade[] expectedTeammember2Grades = new Grade[2];
-//        expectedTeammember2Grades[0] = Grade.builder()
-//                .username("t2chenpa")
-//                .course("CSC207")
-//                .grade(81)
-//                .build();
-//        expectedTeammember2Grades[1] = Grade.builder()
-//                .username("t2chenpa")
-//                .course("CSC148")
-//                .grade(89)
-//                .build();
-//
-//        Availability expectedTeam = Availability.builder()
-//                .name("team1")
-//                .members(new String[]{"t1chenpa", "t2chenpa"})
-//                .build();
-//
-//
-//
-//        // Define the behavior of the mock
-//        when(gradeDB.getGrade("t1chenpa", "CSC207")).thenReturn(expectedTeammember1Grades[0]);
-//        when(gradeDB.getGrade("t2chenpa", "CSC207")).thenReturn(expectedTeammember2Grades[0]);
-//
-//        when(gradeDB.getGrades("t1chenpa")).thenReturn(expectedTeammember1Grades);
-//        when(gradeDB.getGrades("t2chenpa")).thenReturn(expectedTeammember2Grades);
-//        when(gradeDB.getMyTeam()).thenReturn(expectedTeam);
-//
-//        // Act
-//        float result = getAverageGradeUseCase.getAverageGrade("CSC207");
-//
-//        // Assert
-//        assertEquals(83.0, result);
     }
 }
