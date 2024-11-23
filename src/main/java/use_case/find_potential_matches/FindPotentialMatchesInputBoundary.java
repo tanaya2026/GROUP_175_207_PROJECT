16b17ca3d3d11a0b5d5f8e4b1cff28201a69de84
cbolinius <cooperbolinius@gmail.com>
2024-11-23 16:23:52 -0500

    Added CA files for the Find Potential Matches use case and begun implementation. Deleted UserDataBase.java since no longer needed (using DataAccessObject.java only instead). Renamed DBUserDataAccessObject.java to DONOTUSEDAO.java since it is to be deleted but we may still need some code from it. Made DataAccessObject implement all of the various DataAccessInterfaces as it should for CA.

---
+++ src/main/java/use_case/find_potential_matches/FindPotentialMatchesInputBoundary.java
@@ -0,0 +1,14 @@
+package use_case.find_potential_matches;
+
+/**
+ * The Find Potential Matches Use Case.
+ */
+public interface FindPotentialMatchesInputBoundary {
+
+    /**
+     * Execute the Find Potential Matches Use Case.
+     * @param findPotentialMatchesInputData the input data for this use case
+     */
+    void execute(FindPotentialMatchesInputData findPotentialMatchesInputData);
+
+}
