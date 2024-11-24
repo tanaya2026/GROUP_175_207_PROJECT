ad3b97d2c75a41ecc18d95153d497dce439e1ed3
Tanaya <tanaya.datar@mail.utoronto.ca>
2024-11-23 22:07:49 -0500

    Changed build method in AppBuilder, Included addHomePageView method in AppBuilder, added HomePage package in interface adapter

---
+++ src/main/java/interface_adapter/homepage/HomePageViewModel.java
@@ -0,0 +1,25 @@
+package interface_adapter.homepage;
+
+import interface_adapter.ViewModel;
+import interface_adapter.find_potential_matches.FindPotentialMatchesState;
+
+/**
+ * The View Model for HomePage View.
+ */
+public class HomePageViewModel extends ViewModel<HomePageViewState> {
+
+    public static final String TITLE_LABEL = "StudyBuddyFinder App";
+    public static final String USERNAME_LABEL = "Insert username";
+    public static final String PASSWORD_LABEL = "Insert password";
+    public static final String APP_LABEL = "Hello! Welcome to StudyBuddyFinder! Our application aims to pair students with study partners, who have similar courses, programs, and availability to enhance academic performance of our users.\n";
+
+    public static final String SIGNUP_BUTTON_LABEL = "Create an Account";
+    public static final String LOGIN_TITLE_BUTTON_LABEL = "Login";
+
+
+    public HomePageViewModel() {
+        super("home page");
+        setState(new HomePageViewState());
+    }
+
+}
---
+++ src/main/java/interface_adapter/homepage/HomePageViewState.java
@@ -0,0 +1,8 @@
+package interface_adapter.homepage;
+
+/**
+ * The state for the HomePage View Model.
+ */
+
+public class HomePageViewState {
+}
