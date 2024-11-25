95013d9a20f8f9e41885e1aea9f6dad0a5d1cccd
Chang Jinbo <changj78@teach.cs.toronto.edu>
2024-11-25 06:48:25 -0500

    change edit_profile usecases based on changes in DAO, and added more personal information to be edited and added some logic to deal with some exceptional scenarios

--- src/main/java/use_case/edit_profile/EditProfileInputBoundary.java
+++ src/main/java/use_case/edit_profile/EditProfileInputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.edit_profile;
 
 public interface EditProfileInputBoundary {
-    void editProfile(EditProfileInputData inputData);
+    void editProfile(String username, EditProfileInputData inputData);
 }
--- src/main/java/use_case/edit_profile/EditProfileInputData.java
+++ src/main/java/use_case/edit_profile/EditProfileInputData.java
@@ -1,17 +1,38 @@
 package use_case.edit_profile;
 
+import entity.Course;
 import entity.Timeslot;
+import java.util.List;
 import java.util.Map;
 
 public class EditProfileInputData {
+    private final String email;
+    private final String password;
     private final String name;
     private final String bio;
-    private final Map<Timeslot, Boolean> availability;
+    private final String program;
+    private final List<Course> courses;
+    private final Map<Timeslot, Boolean> availability; // Updated availability
+    private final String schedulerID; // Needed for deleting the current schedule
 
-    public EditProfileInputData(String name, String bio, Map<Timeslot, Boolean> availability) {
+    public EditProfileInputData(String email, String password, String name, String bio, String program,
+                                List<Course> courses, Map<Timeslot, Boolean> availability, String schedulerID) {
+        this.email = email;
+        this.password = password;
         this.name = name;
         this.bio = bio;
+        this.program = program;
+        this.courses = courses;
         this.availability = availability;
+        this.schedulerID = schedulerID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getName() {
@@ -22,7 +43,19 @@ public class EditProfileInputData {
         return bio;
     }
 
+    public String getProgram() {
+        return program;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
     public Map<Timeslot, Boolean> getAvailability() {
         return availability;
+    }
+
+    public String getSchedulerID() {
+        return schedulerID;
     }
 }
