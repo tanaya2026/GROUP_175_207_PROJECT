b08b45e356215e941b4f134aa97d0de36e16cf12
Chang Jinbo <changj78@teach.cs.toronto.edu>
2024-12-02 09:36:34 -0500

    edit some mistakes

--- src/main/java/use_case/edit_profile/EditProfileInputBoundary.java
+++ src/main/java/use_case/edit_profile/EditProfileInputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.edit_profile;
 
 public interface EditProfileInputBoundary {
-    void editProfile(String username, EditProfileInputData inputData);
+    void editProfile(EditProfileInputData inputData);
 }
